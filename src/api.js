93580b56b937a63673741af7456a492910bc42dc
Matthew Nolan <mnolan132@gmail.com>
2022-10-14 21:24:10 +1300

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8b9f776b0bmsh6bea930f13fd41fp1d82c7jsn470f96eafbf6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0bceb25dcb391b4c640471815e997393";
